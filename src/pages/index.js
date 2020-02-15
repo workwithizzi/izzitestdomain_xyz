7c2adc4078edad1c14421ec055083fbfa6a50b37
IvaTsu <ivan.tsud@gmail.com>
2020-02-15 22:27:16 +0200

    Edit text 17

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>IZZI</h1>
+    <h1>IZZI</h1>
+    <h1>IZZI</h1>
+    <h1>IZZI</h1>
+    <h1>IZZI</h1>
     <Link to="/page-2/">Page 2!</Link>
   </Layout>
 )
